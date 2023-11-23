4bcf189df7a495481a7c59fd94462bd425d9c343
Theo-Nicolas63 <theo.nicolas63@gmail.com>
2023-11-23 17:46:42 +0100

    [DEV][NOT WORKING] debut postcollection

--- Rating/internal/controllers/collections/post_collection.go
+++ Rating/internal/controllers/collections/post_collection.go
@@ -9,20 +9,11 @@ import (
 	"net/http"
 )
 
-// GetCollection
-// @Tags         collections
-// @Summary      Get a collection.
-// @Description  Get a collection.
-// @Param        id           	path      string  true  "Collection UUID formatted ID"
-// @Success      200            {object}  models.Collection
-// @Failure      422            "Cannot parse id"
-// @Failure      500            "Something went wrong"
-// @Router       /collections/{id} [get]
-func GetCollection(w http.ResponseWriter, r *http.Request) {
+func PostCollection(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	collectionId, _ := ctx.Value("collectionId").(uuid.UUID)
 
-	collection, err := collections.GetCollectionById(collectionId)
+	//collection, err := collections.GetCollectionById(collectionId)
 	if err != nil {
 		logrus.Errorf("error : %s", err.Error())
 		customError, isCustom := err.(*models.CustomError)
